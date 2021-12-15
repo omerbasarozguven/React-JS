992611627d38f081c3f64d71aa00822729ba0067
bronixEngineering <bronixengineering@gmail.com>
2021-12-15 16:27:10 +0300

    second commit

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends React.Component {
-  foo = () => "Bars";
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     const number = "omer";
     return (
@@ -12,7 +23,7 @@ class App extends React.Component {
         <h1>Hello</h1>
         <Navbar title={number} />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- github_finder/src/components/layout/Navbar.js
+++ github_finder/src/components/layout/Navbar.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-  };
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className="fab fa-github" />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({title}) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className="fab fa-github" />
+        {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Navbar;
