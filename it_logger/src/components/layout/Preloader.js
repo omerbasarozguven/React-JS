bf55823e0a132a03fb8ffc6c689754330d98c273
bronixEngineering <bronixengineering@gmail.com>
2021-12-16 19:57:28 +0300

    redux project added

---
+++ it_logger/src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = (props) => {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="intermediate blue" />
+    </div>
+  );
+};
+
+export default Preloader;
